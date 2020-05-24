1e5f08b4555f9789bdd7825e8cdd5284aeaea964
Keith Ott <ducttape12@yahoo.com>
2020-05-24 09:10:30 -0500

    Updated icon

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChartLine } from '@fortawesome/free-solid-svg-icons';
+import { faChartBar } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
 
 export default class Navigation extends React.Component {
     render() {
         return (
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="/"><FontAwesomeIcon icon={faChartLine} /> {this.props.brand}</Navbar.Brand>
+                <Navbar.Brand href="/"><FontAwesomeIcon icon={faChartBar} /> {this.props.brand}</Navbar.Brand>
             </Navbar>
         );
     }
